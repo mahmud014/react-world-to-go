a1bf7face80b797b27811a4b93997219abd9d503
mahmud014 <anik955720@gmail.com>
2025-09-20 19:27:06 +0600

    Ready for projects

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./assets/components/countries/Countries";
+
+function App() {
+  return (
+    <>
+      <h1>Wolcome to world React</h1>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
